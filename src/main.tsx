ea36196ce8979191afc6a3ebe5642eb31ab734ab
phat.huy <phat.huy97@gmail.com>
2023-06-17 15:09:59 +0700

    chore: configure absolute import and add theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "src/contents/home/Home.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-      <div>test</div>
-  </React.StrictMode>,
-)
+    <Home />
+  </React.StrictMode>
+);
