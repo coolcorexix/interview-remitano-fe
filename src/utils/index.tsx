fdd8bb8daaa7a74d20b04ce43c0c2be45ad47466
phatIntern <phat.pham@saleshood.com>
2023-06-18 21:23:09 +0700

    feat: add random color

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -58,4 +58,19 @@ function numberToReadableString(views: string): string {
   return views.toLocaleString();
 }
 
+export function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  if (color === "#FFFFFF") {
+    color = "#000000";
+  }
+
+  return color;
+}
+
 export { formatTimeAgo, numberToReadableString };
